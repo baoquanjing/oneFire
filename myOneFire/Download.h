5c8ca7c04476296ced3e95cbc279c9a787ffdbfb
jbq1232006 <jbq1232006@126.com>
2015-06-02 10:18:37 +0800

    add new class
    add new class download for download

--- myOneFire/Download.h
+++ myOneFire/Download.h
@@ -1,14 +1,13 @@
 //
-//  DownloadManager.h
+//  Download.h
 //  myOneFire
 //
-//  Created by ngds on 15/5/25.
+//  Created by ngds on 15/6/2.
 //  Copyright (c) 2015年 ngds. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-#import "Download.h"
 
-@interface DownloadManager : NSObject
+@interface Download : NSObject
 
 @end
---
+++ myOneFire/Download.m
@@ -0,0 +1,13 @@
+//
+//  Download.m
+//  myOneFire
+//
+//  Created by ngds on 15/6/2.
+//  Copyright (c) 2015年 ngds. All rights reserved.
+//
+
+#import "Download.h"
+
+@implementation Download
+
+@end
